20cbe07d64b33b535b99af76c1946f7479695d69
ThiraphatDotSa <thiraphat_120@hotmail.com>
2025-10-02 00:44:08 +0700

    delete App.css and fix index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import baseUrl from '../constants/ServerConstant'
 import axios from 'axios'
 import AppRoutes from './routes/AppRoutes'
